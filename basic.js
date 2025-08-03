acf8972a5c0f1b2d74c902fe7a07346a1a8c79f9
Milja <kaledriina@gmail.com>
2025-08-03 18:42:01 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -9,21 +9,6 @@ window.onload = () => {
                return markers;
             }
 
-    function distance(lat1, lon1, lat2, lon2) {
-        const r = 6371; // km
-        const p = Math.PI / 180;
-
-        const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
-                + Math.cos(lat1 * p) * Math.cos(lat2 * p) *
-                  (1 - Math.cos((lon2 - lon1) * p)) / 2;
-
-        return 2 * r * Math.asin(Math.sqrt(a));
-        }
-
-    function calculate_distances(lon, lat, markers) {
-            return;
-        }
-
     el.addEventListener("gps-camera-update-position", e => {
         if(!markersAdded) {
             const initial_lat = ${e.detail.position.latitude};
@@ -36,19 +21,6 @@ window.onload = () => {
         markersAdded = true;
         alert markers;
             
-
-const entity = document.createElement("a-box");
-            entity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'red' } );
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            });
-            document.querySelector("a-scene").appendChild(entity);
         }
 
     });
