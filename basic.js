772a2005fab923cccc16607edce2d899629a97ed
Milja <kaledriina@gmail.com>
2025-08-03 18:56:13 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -3,15 +3,26 @@ window.onload = () => {
 
     const el = document.querySelector("[gps-new-camera]");
 
-    el.addEventListener("gps-camera-update-position", async(e) => {
+    el.addEventListener("gps-camera-update-position", e => {
         if(!markersAdded) {
             const initial_lat = e.detail.position.latitude;
             const initial_lon = e.detail.position.longitude; 
             alert(`Got first GPS position: lon lat`);
             // Add a box to the north of the initial GPS position
-             
+           const entity = document.createElement("a-box");
+            entity.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            entity.setAttribute('material', { color: 'red' } );
+            entity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            });
+            document.querySelector("a-scene").appendChild(entity);
         }
-
+        markersAdded = true;
     });
 };
 
