0434671273c60bc98e82df129047f662b14ac764
Milja <kaledriina@gmail.com>
2025-08-03 18:52:18 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -3,12 +3,6 @@ window.onload = () => {
 
     const el = document.querySelector("[gps-new-camera]");
 
-    function createMarkers(lat, lon) { 
-                alert('Marker function start');
-                var markers = 1;
-                return markers;
-            }
-
     el.addEventListener("gps-camera-update-position", async(e) => {
         if(!markersAdded) {
             const initial_lat = e.detail.position.latitude;
