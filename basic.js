266dabaa006b7c7262fdc106342795387b4492c2
Milja <kaledriina@gmail.com>
2025-08-03 18:59:48 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -9,18 +9,7 @@ window.onload = () => {
             const initial_lon = e.detail.position.longitude; 
             alert(`Got first GPS position: lon ${initial_lon} lat ${initial_lat}`);
             // Add a box to the north of the initial GPS position
-           const entity = document.createElement("a-box");
-            entity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'red' } );
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            });
-            document.querySelector("a-scene").appendChild(entity);
+
         }
         markersAdded = true;
     });
