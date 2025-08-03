791b5767861a08f4d27f4f5c4bf64261000cc3f9
Milja <kaledriina@gmail.com>
2025-08-03 18:48:17 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -3,20 +3,19 @@ window.onload = () => {
 
     const el = document.querySelector("[gps-new-camera]");
 
-    function createMarkers(beginning_lat, beginning_lon) { 
+    function createMarkers(lat, lon) { 
                 alert('Marker function start');
-                const markers = 1;
-               return markers;
+                var markers = 1;
+                return markers;
             }
 
-    el.addEventListener("gps-camera-update-position", e => {
+    el.addEventListener("gps-camera-update-position", async(e) => {
         if(!markersAdded) {
-            const initial_lat = ${e.detail.position.latitude};
-            
-            const initial_lon = ${e.detail.position.longitude}; 
+            const initial_lat = e.detail.position.latitude;
+            const initial_lon = e.detail.position.longitude; 
             alert(`Got first GPS position: lon lat`);
             // Add a box to the north of the initial GPS position
-        const markers = createMarkers(initial_lat, initial_lon);
+        var markers = createMarkers(initial_lat, initial_lon);
         alert('Markers created!');
         markersAdded = true;
         alert markers;
