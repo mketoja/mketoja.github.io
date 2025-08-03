97f9d81fcc18efe836638844738b28e992a36f3a
Milja <kaledriina@gmail.com>
2025-08-03 19:33:40 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -2,13 +2,38 @@ window.onload = () => {
     let markersAdded = false;
 
     function createMarkers(initial_lat, initial_lon) { 
-                alert('Marker function start');
-                const markers = [{lat: initial_lat + 0.001,
-                                  lon: initial_lon }, 
-                                  {lat: initial_lat,
+                const markers = [{name: a,
+                                  lat: initial_lat + 0.001,
+                                  lon: initial_lon },
+ 
+                                  {name: b, 
+                                  lat: initial_lat,
                                   lon: initial_lon + 0.001 }];
+                alert('Markers added');
                 return markers;
-            }    
+            }   
+
+    function distance(lat1, lon1, lat2, lon2) {
+        const r = 6371; // km
+        const p = Math.PI / 180;
+
+        const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
+                + Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+                  (1 - Math.cos((lon2 - lon1) * p)) / 2;
+
+        return 2 * r * Math.asin(Math.sqrt(a));
+        } 
+
+    function calculate_distances(lon, lat, markers) {
+        for (let i = 0; i < markers.length; i++) {
+            Alert('Calculating marker distance');
+            marker = markers[i];
+            distance = distance(lat, lon, marker.lat, marker.lon);
+            if (distance == 0) {
+                alert('Found marker!' + marker.name);
+            }
+            return;
+        }
 
     const el = document.querySelector("[gps-new-camera]");
 
@@ -21,7 +46,7 @@ window.onload = () => {
             markers = createMarkers(initial_lat, initial_lon);
             alert('Markers added');
             markersAdded = true;
-            alert markers;
+
         }
 
     });
