fb2a18ec9145857f4520ba61e016ad2164a0ff86
Milja <kaledriina@gmail.com>
2025-08-03 18:39:36 +0300

    Initial stuff

--- basic.js
+++ basic.js
@@ -5,22 +5,13 @@ window.onload = () => {
 
     function createMarkers(beginning_lat, beginning_lon) { 
                 alert('Marker function start');
-                const markers = [
-                    {lat: beginning_lat + 0.001,
-                     long: beginning_long,
-                     event: "x" 
-                     },
-                    {lat: beginning_lat,
-                     long: beginning_long + 0.001
-                     event: "y"
-                    }
-                ];
+                const markers = 1;
                return markers;
             }
 
     function distance(lat1, lon1, lat2, lon2) {
         const r = 6371; // km
-         const p = Math.PI / 180;
+        const p = Math.PI / 180;
 
         const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
                 + Math.cos(lat1 * p) * Math.cos(lat2 * p) *
@@ -36,6 +27,7 @@ window.onload = () => {
     el.addEventListener("gps-camera-update-position", e => {
         if(!markersAdded) {
             const initial_lat = ${e.detail.position.latitude};
+            
             const initial_lon = ${e.detail.position.longitude}; 
             alert(`Got first GPS position: lon lat`);
             // Add a box to the north of the initial GPS position
